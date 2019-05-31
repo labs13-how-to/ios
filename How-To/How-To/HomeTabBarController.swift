a4f6e63892e804f9823f7b2a12a9e7334ce20d97
Angel <angelbuenrostro@outlook.com>
2019-05-31 17:26:09 -0400

    Change the app delegate so that root view controller is the HomeTabBarController

---
+++ How-To/How-To/HomeTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabBarController.swift
+//  How-To
+//
+//  Created by Angel Buenrostro on 5/31/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9993286729, green: 0.7073625326, blue: 0.4233144522, alpha: 1)
+    }
+}
